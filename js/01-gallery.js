0673300f770785709eabf72d03fca8c8e2c3b4fa
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2023-08-09 01:23:58 +0300

    add task -01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector(`.gallery`);
+
+function createMarcup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join(``);
+}
+
+container.insertAdjacentHTML(`beforeend`, createMarcup(galleryItems));
+container.addEventListener(`click`, handlerImgClick);
+
+let instance;
+
+function handlerImgClick(evt) {
+  evt.preventDefault();
+  const bigPicture = evt.target.dataset.source;
+
+  if (instance) {
+    instance.close();
+  }
+
+  instance = basicLightbox.create(`<img src="${bigPicture}"/>`);
+  instance.show();
+}
+
+document.addEventListener("keydown", handlerKey);
+
+function handlerKey(evt) {
+  if (evt.code === "Escape" && instance) {
+    instance.close();
+  }
+}
